5736782cc3ea72cf841cb0bbd8c4d29e3e91e8bc
Hamilton Silva <hamiltonsilva.dev@gmail.com>
2024-06-04 14:19:01 +0100

    Updated

---
+++ 03-modules/project-modal/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>{{ title }}</h1>
+  <br />
+  <input type="text" ref="name" />
+  <button @click="handleClick">Add</button>
+  <Model />
+</template>
+
+<script>
+import Model from './components/Model.vue'
+
+export default {
+  name: 'App',
+  data: () => {
+    return {
+      title : 'Hello World :) !'
+    }
+  }, 
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name.value);
+      this.$refs.name.classList.add('add');
+    }
+  },
+  components: {
+    Model
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
